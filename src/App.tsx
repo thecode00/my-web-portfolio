e8774beb510d19bed42d92159f03fb69b7122bfa
thecode00 <thecode0087@gmail.com>
2022-11-02 23:25:25 +0900

    App initialize

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import TriangleCanvas from "./components/TriangleCanvas";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,8 +18,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
+      </header> */}
+      <TriangleCanvas />
+    </>
   );
 }
 
